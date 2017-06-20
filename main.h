78b34964d5e0f20e7602877831d3a03e92feb0f8
tybe_NmLa_C <mrmrmrm2m3m4@gmail.com>
2017-06-20 16:38:28 +0200

    Update and rename main.md to main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <windows.h.>
+
+#define DLL_EXPORT __declspec(dllexport)
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	void DLL_EXPORT FuncOpenDLL();
+
+}
+#endif
+
