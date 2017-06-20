a2626d7b3b3ffbee19c1ed283e8c8fd699167caf
tybe_NmLa_C <mrmrmrm2m3m4@gmail.com>
2017-06-20 16:50:18 +0200

    Create main.h

---
+++ func/main.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <windows.h.>
+
+#define DLL_EXPORT __declspec(dllexport)
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	void DLL_EXPORT FuncOpenDLL();
+
+}
+#endif
